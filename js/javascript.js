3694ea90db2a9137485ae375571f2d53d2945287
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-19 22:15:48 -0700

    fetch research

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,43 @@
-Java;
+// score code needs to be pulled
+
+var apiKey = "359d18be100f928817fd7d3a21693376";
+
+var myHeaders = new Headers();
+myHeaders.append("x-api-key", "359d18be100f928817fd7d3a21693376");
+
+var requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+fetch("https://api.documenu.com/v2/restaurant/4072702673999819", requestOptions)
+  .then((response) => response.text())
+  .then((result) => window.$log.trace(result))
+  .catch((error) => console.error("error", error));
+
+var getUserCategory = function (category) {
+  var apiUrl =
+    "https://api.documenu.com/v2/restaurants/zip_code/97403?size=20&page=1&exact=true&cuisine=mexican&top_cuisines=true?key=359d18be100f928817fd7d3a21693376";
+
+  // make a get request to url
+  fetch(
+    "https://api.documenu.com/v2/restaurant/4072702673999819",
+    requestOptions
+  )
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Documenu");
+    });
+};

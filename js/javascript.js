a840a3270b39a227c04b9245cd07b8294bf6ce5a
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-19 18:52:15 -0700

    score button added

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,12 @@
-Java;
+var score = 10;
+
+var scoreButtonEl = document.querySelector("#submiter"); //!Need submit button id.
+var scoreEl = document.querySelector("#score");
+
+scoreButtonEl.addEventListener("click", function () {
+  event.preventDefault();
+  if (score > 0) {
+    score--;
+    scoreEl.textContent = score;
+  }
+});

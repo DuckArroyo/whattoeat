cc132269a06254d80089bf8518fa43eda88dfa67
Q is Coding <quintenbuck@icloud.com>
2021-09-22 19:13:36 -0700

    Worked on building cards

--- js/javascript.js
+++ js/javascript.js
@@ -39,31 +39,59 @@ function buildcard(data) {
 getRestaurant(Restaurant, State); //remove later
 
 //Edamam fetch
-function getEdamam() {
-  var stackeduserentry = document.querySelector("#stacked-userentry").value;
+function edmamFetch (category, state){
+  fetch("https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
+  +state+"?cuisine="+category+"&api_key" +
+  "d5705ed4f7msh48cd378e1f7bdcfp184776jsnd69703bb9e74")
+  .then(
+   function(response){
+     return response.json()
+   }
+ ).then(
+   function(data){
+     console.log(data)
+     buildcard(data)
+   }
+ )
+ numOfCards++;
+ var rowNum = Math.ceil(numOfCards / 3);
 
-  fetch(
-    "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?q=" +
-      stackeduserentry +
-      "&api_key" +
-      "d5705ed4f7msh48cd378e1f7bdcfp184776jsnd69703bb9e74"
-  )
-    .then((response) => response.json())
-    .then(data);
-  console.log(data);
-}
-      .then(function (response) {
-      console.log(response.data[0]);
+ var newimage = $("<img>")
+          .attr("src", response.articles[0].urlToImage)
+          .addClass("imgFit");
 
-        .catch(err => {console.error(err);})
+          var newDiv = $("<div>").addClass("edamam");
+          newDiv.append(newimage);
 
-        var containerEl = document.querySelector("#box"); //! edamam or edamam response in html
+          var header = $("<h2>")
+          .addClass("post-header")
+          .text(response.articles[0].title);
 
-        containerEl.innerHTML = null;
-        var imgEl = document.createElement("img");
-        imgEl.setAttribute("src");
-        containerEl.appendChild(imgEl);
-      });
-      }
+          var summary = $("<p>").text(response.articles[0].description);
+          var categoryTitle = $("<span>")
+            .addClass("category")
+            .text(response.articles[0].source.name);
 
+            var linktosite = $("<a>")
+            .addClass("btn btn-primary siteLink")
+            .attr("href", response.articles[0].url)
+            .attr("target", "_blank")
+            .text("go to article")
+            .attr("id", response.articles[0].title);
+    
+          var newsContentDiv = $("<div>").addClass("news-content");
+          newsContentDiv
+            .append(categoryTitle)
+            .append(header)
+            .append(summary)
+            .append(linktosite);
+            
+            var cardDiv = $("<div>").addClass("cards");   
+            cardDiv.append(newDiv).append(newsContentDiv);
+
+            var responsiveDiv = $("<div>").addClass("col-sm-4");
+            responsiveDiv.append(cardDiv);
+            $(".cardRow" + rowNum).append(responsiveDiv);
+  
 var btn = document.getElementById("#locate"); //! Still good
+}

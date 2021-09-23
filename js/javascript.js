ec6d3bb9c6b4d1ea7860883e341c1c12e07a4df1
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-22 19:58:14 -0700

    merge from Quinten

--- js/javascript.js
+++ js/javascript.js
@@ -36,62 +36,66 @@ function buildcard(data) {
   //! Bryan write your code here
 }
 
-getRestaurant(Restaurant, State); //remove later
+//getRestaurant(Restaurant, State); //remove later
 
 //Edamam fetch
-function edmamFetch (category, state){
-  fetch("https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
-  +state+"?cuisine="+category+"&api_key" +
-  "d5705ed4f7msh48cd378e1f7bdcfp184776jsnd69703bb9e74")
-  .then(
-   function(response){
-     return response.json()
-   }
- ).then(
-   function(data){
-     console.log(data)
-     buildcard(data)
-   }
- )
- numOfCards++;
- var rowNum = Math.ceil(numOfCards / 3);
+function edmamFetch(category, state) {
+  fetch(
+    "https://edamam-food-and-grocery-database.p.rapidapi.com/parser" +
+      state +
+      "?cuisine=" +
+      category +
+      "&api_key" +
+      "d5705ed4f7msh48cd378e1f7bdcfp184776jsnd69703bb9e74"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      buildcard(data);
+    });
+  //numOfCards++;
+  // var rowNum = Math.ceil(numOfCards / 3);
 
- var newimage = $("<img>")
-          .attr("src", response.articles[0].urlToImage)
-          .addClass("imgFit");
+  var newimage = $("<img>")
+    .attr("src", response.articles[0].urlToImage)
+    .addClass("imgFit");
 
-          var newDiv = $("<div>").addClass("edamam");
-          newDiv.append(newimage);
+  var newDiv = $("<div>").addClass("edamam");
+  newDiv.append(newimage);
 
-          var header = $("<h2>")
-          .addClass("post-header")
-          .text(response.articles[0].title);
+  var header = $("<h2>")
+    .addClass("post-header")
+    .text(response.articles[0].title);
 
-          var summary = $("<p>").text(response.articles[0].description);
-          var categoryTitle = $("<span>")
-            .addClass("category")
-            .text(response.articles[0].source.name);
+  var summary = $("<p>").text(response.articles[0].description);
+  var categoryTitle = $("<span>")
+    .addClass("category")
+    .text(response.articles[0].source.name);
 
-            var linktosite = $("<a>")
-            .addClass("btn btn-primary siteLink")
-            .attr("href", response.articles[0].url)
-            .attr("target", "_blank")
-            .text("go to article")
-            .attr("id", response.articles[0].title);
-    
-          var newsContentDiv = $("<div>").addClass("news-content");
-          newsContentDiv
-            .append(categoryTitle)
-            .append(header)
-            .append(summary)
-            .append(linktosite);
-            
-            var cardDiv = $("<div>").addClass("cards");   
-            cardDiv.append(newDiv).append(newsContentDiv);
+  var linktosite = $("<a>")
+    .addClass("btn btn-primary siteLink")
+    .attr("href", response.articles[0].url)
+    .attr("target", "_blank")
+    .text("go to article")
+    .attr("id", response.articles[0].title);
 
-            var responsiveDiv = $("<div>").addClass("col-sm-4");
-            responsiveDiv.append(cardDiv);
-            $(".cardRow" + rowNum).append(responsiveDiv);
-  
-var btn = document.getElementById("#locate"); //! Still good
+  var newsContentDiv = $("<div>").addClass("news-content");
+  newsContentDiv
+    .append(categoryTitle)
+    .append(header)
+    .append(summary)
+    .append(linktosite);
+
+  var cardDiv = $("<div>").addClass("cards");
+  cardDiv.append(newDiv).append(newsContentDiv);
+
+  var responsiveDiv = $("<div>").addClass("col-sm-4");
+  responsiveDiv.append(cardDiv);
+  $(".cardRow" + rowNum).append(responsiveDiv);
+
+  var btn = document.getElementById("#locate"); //! Still good
 }
+
+edmamFetch();

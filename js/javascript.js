a05b1daf8e440da548e526dd98b1349ede139119
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-22 22:42:41 -0700

    success with Documenu API response, hard coded

--- js/javascript.js
+++ js/javascript.js
@@ -13,22 +13,19 @@ var edamamResponseEl = document.querySelector("#edamam-response");
 var documenuResponseEl = document.querySelector("#documenu-response");
 
 //Documenu fetch
-function getDocumenu(category, state) {
+function getDocumenu() {
   fetch(
-    "https://api.documenu.com/v2/restaurants/state/" +
-      state +
-      "?cuisine=" +
-      category +
-      "&key=0a3d92451c7ba73ea09adb2814dd9649"
+    "https://api.documenu.com/v2/restaurants/search/fields?state=oregon&cuisine=mexican&top_cuisines=false&zip_code=97403&key=359d18be100f928817fd7d3a21693376"
   )
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-      console.log(data);
-      //buildcard(data)
+    .then(function (answer) {
+      buildDocumenuCard(answer);
     });
 }
+
+getDocumenu();
 
 //Edamam fetch
 function getEdamam() {
@@ -48,14 +45,19 @@ function getEdamam() {
     });
 }
 
-//getEdamam(user, category); //remove when buttons are ready
+//getEdamam(); //remove when buttons are ready
 
-function buildDocumenuCard(data) {
-  console.log(data);
-  var title = data.data[i].restaurant_name;
-  var htmlTitle = $("h1").text(title);
-  $("#documenu").append(htmlTitle);
-  //! Bryan write your code here
+function buildDocumenuCard(answer) {
+  console.log(answer.data);
+  console.log(answer.data[0]);
+  for (i = 0; i <= answer.data.length; i++) {
+    console.log(answer.data[i].restaurant_name);
+    //Container for each card
+    var card = document.createElement("div");
+    card.setAttribute("id", "restaurantDiv");
+    card.setAttribute("class", "card");
+    edamamResponseEl.appendChild(card);
+  }
 }
 
 function buildEdamamCard(data) {
@@ -111,8 +113,8 @@ function buildEdamamCard(data) {
 
 var search = document.getElementById("#locate"); //! Still good
 
-getEdamamEl.addEventListener("click", getEdamam);
-getDocumenuEl.addEventListener("click", getDocumenu);
+//getEdamamEl.addEventListener("click", getEdamam);
+//getDocumenuEl.addEventListener("click", getDocumenu);
 scoreButtonEl.addEventListener("click", function () {
   event.preventDefault();
   console.log("Search button pressed");
